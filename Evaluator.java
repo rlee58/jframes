d34749cd97358de842f355925b3b598e8d9e1733
rlee58 <42381818+rlee58@users.noreply.github.com>
2019-03-10 22:02:48 -0400

    Update Evaluator.java

--- Evaluator.java
+++ Evaluator.java
@@ -3,43 +3,132 @@ import java.util.ArrayList;
 
 public class Evaluator {
 	
-	private ArrayList<Integer> cpuValue = new ArrayList<Integer>();
-	private ArrayList<Integer> cpuValue2 = new ArrayList<Integer>();
 	private ArrayList<Integer> playerValue = new ArrayList<Integer>();
-	private ArrayList<Integer> playerValue2 = new ArrayList<Integer>();
-	private int[] cpuTotal = new int[2];
-	private int[] playerTotal = new int[2];
+	private ArrayList<Integer> cpuValue = new ArrayList<Integer>();
+	private int[] playerTotal = new int[5];
+	private int[] cpuTotal = new int[5];
 	
-	public Evaluator(ArrayList<Integer> cpu,ArrayList<Integer> player){
+	public Evaluator(ArrayList<Integer> player,ArrayList<Integer> cpu){
 		cpuValue = cpu;
 		playerValue = player;
-		cpuValue2 = cpu;
-		playerValue2 = player;
-		for(int i=0;i<cpuValue.size();i++){
-			if(cpuValue.get(i)>10){
-				cpuValue.set(i, 10);
-			}
-			if(cpuValue.get(i)==1){
-				cpuValue2.set(i, 11);
-			}
-		}for(int i=0;i<playerValue.size();i++){
+		for(int i=0;i<playerValue.size();i++){
 			if(playerValue.get(i)>10){
 				playerValue.set(i, 10);
 			}
-			if(playerValue.get(i)==11){
-				playerValue2.set(i, 11);
+		}for(int i=0;i<cpuValue.size();i++){
+			if(cpuValue.get(i)>10){
+				cpuValue.set(i, 10);
 			}
 		}
 	}
 	
 	public void evaluate(){
-		int cpuTotal1=0;
-		int cpuTotal2=0;
-		for(int i=0;i<cpuValue.size();i++){
-			cpuTotal1+=cpuValue.get(i);
+		int playerCount = 0;
+		int playerAceCount = 0;
+		for(int i=0;i<playerValue.size();i++){
+			playerCount+=playerValue.get(i);
 		}
-		for(int i=0;i<cpuValue.size();i++){
-			cpuTotal2+=cpuValue2.get(i);
+		playerTotal[0]=playerCount;
+		
+		for(int i=0;i<playerValue.size();i++){
+			if(playerValue.get(i)==1){
+				playerAceCount++;
+			}
 		}
+		if(playerAceCount==4){
+			int ten = 0;
+			for(int i=1;i<=4;i++){
+				playerTotal[i]=playerCount;
+				ten+=10;
+				playerTotal[i]+=ten;
+			}
+		}
+		if(playerAceCount>=3){
+			int ten = 0;
+			for(int i=1;i<=3;i++){
+				playerTotal[i]=playerCount;
+				ten+=10;
+				playerTotal[i]+=ten;
+			}
+		}
+		if(playerAceCount>=2){
+			int ten = 0;
+			for(int i=1;i<=2;i++){
+				playerTotal[i]=playerCount;
+				ten+=10;
+				playerTotal[i]+=ten;
+			}
+		}
+		if(playerAceCount>=1){
+			playerTotal[1]=playerCount+10;
+		}
+		
+		int cpuCount = 0;
+		int cpuAceCount = 0;
+		for(int i=0;i<cpuValue.size();i++){
+			cpuCount+=cpuValue.get(i);
+		}
+		cpuTotal[0]=cpuCount;
+		
+		for(int i=0;i<cpuValue.size();i++){
+			if(cpuValue.get(i)==1){
+				cpuAceCount++;
+			}
+		}
+		if(cpuAceCount==4){
+			int ten = 0;
+			for(int i=1;i<=4;i++){
+				cpuTotal[i]=cpuCount;
+				ten+=10;
+				cpuTotal[i]+=ten;
+			}
+		}
+		if(cpuAceCount>=3){
+			int ten = 0;
+			for(int i=1;i<=3;i++){
+				cpuTotal[i]=cpuCount;
+				ten+=10;
+				cpuTotal[i]+=ten;
+			}
+		}
+		if(cpuAceCount>=2){
+			int ten = 0;
+			for(int i=1;i<=2;i++){
+				cpuTotal[i]=cpuCount;
+				ten+=10;
+				cpuTotal[i]+=ten;
+			}
+		}
+		if(cpuAceCount>=1){
+			cpuTotal[1]=cpuCount+10;
+		}
+		//check
+		for(int i=0;i<5;i++){
+			System.out.print(cpuTotal[i]+" ");
+		}
+		System.out.println("");
+		for(int i=0;i<5;i++){
+			System.out.print(playerTotal[i]+" ");
+		}
+		System.out.println("");
 	}
+	public void updateValue(ArrayList<Integer> arr,int turn){
+		for(int i=0;i<arr.size();i++){
+			if(arr.get(i)>10){
+				arr.set(i, 10);
+			}
+		}
+		if(turn==0){
+			playerValue=arr;
+		}else if(turn==1){
+			cpuValue=arr;
+		}
+		evaluate();
+	}
+	public int getPlayerTotal(int i){
+		return playerTotal[i];
+	}
+//	public int[] getCpuTotal(){
+//		return cpuTotal;
+//	}
 }
