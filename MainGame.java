c85d652941bb4e432935db5b0429788f231f02c6
rlee58 <42381818+rlee58@users.noreply.github.com>
2019-03-14 17:23:36 -0400

    Update MainGame.java

--- MainGame.java
+++ MainGame.java
@@ -40,8 +40,14 @@ public class MainGame implements ActionListener{
 	private JLabel cpuSlot5;
 	private JLabel cpuSlot6;
 	private JLabel cpuSlot7;
-	JLabel cpuTurn;
+	private JLabel cpuTurn;
+	private JLabel bust;
+	private JLabel cpuBust;
 	private JLabel playerTotLbl;
+	private JLabel cpuTotLbl;
+	private JLabel cpuWins;
+	private JLabel cpuHit;
+	private JLabel cpuStand;
 	private String totLbl = "Total: ";
 
 	/**
@@ -147,11 +153,34 @@ public class MainGame implements ActionListener{
 		cpuTurn.setBounds(900, 960, 181, 42);
 		frame.getContentPane().add(cpuTurn);
 
+		bust = new JLabel("Your total is over 21! Bust!");
+		bust.setBounds(900, 990, 164, 25);
+		frame.getContentPane().add(bust);
+
+		cpuWins = new JLabel("CPU wins!");
+		cpuWins.setBounds(900, 1020, 63, 22);
+		frame.getContentPane().add(cpuWins);
+
+		cpuTotLbl = new JLabel("");
+		cpuTotLbl.setBounds(900, 1050, 112, 42);
+		frame.getContentPane().add(cpuTotLbl);
+
+		cpuHit = new JLabel("CPU hits");
+		cpuHit.setBounds(900, 1080, 56, 16);
+		frame.getContentPane().add(cpuHit);
+
+		cpuStand = new JLabel("CPU stands");
+		cpuStand.setBounds(900, 1080, 73, 22);
+		frame.getContentPane().add(cpuStand);
+
+		cpuBust = new JLabel("CPU total is over 21! You win!");
+		cpuBust.setBounds(900, 1100, 181, 25);
+		frame.getContentPane().add(cpuBust);
+		
 		back();
 
 	}
 	public void back(){
-
 		GameTable one = new GameTable();
 		one.setBounds(0, 13, 782, 731);
 		frame.getContentPane().add(one);
@@ -167,11 +196,19 @@ public class MainGame implements ActionListener{
 	private String playerCardSeven;
 	private String cpuCardOne;
 	private String cpuCardTwo;
+	private String cpuCardThree;
+	private String cpuCardFour;
+	private String cpuCardFive;
+	private String cpuCardSix;
+	private String cpuCardSeven;
 	private ArrayList<String> arr = new ArrayList<String>();
 	private ArrayList<Integer> playerHand = new ArrayList<Integer>();
 	private ArrayList<Integer> cpuHand = new ArrayList<Integer>();
 	private int hitCounter = 0;
+	private int cpuHitCounter = 0;
 	private Evaluator eval;	
+	private int playerHigh;
+	private int cpuHigh;
 
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==btnNewGame){
@@ -215,8 +252,16 @@ public class MainGame implements ActionListener{
 
 
 				hitCounter = 0;
+				cpuHitCounter = 0;
 				btnHit.setBounds(295,700,97,25);
 				btnStand.setBounds(400,700,97,25);
+				bust.setBounds(900, 990, 164, 25);
+				cpuWins.setBounds(900, 1020, 63, 22);
+				cpuTurn.setBounds(900, 960, 181, 42);
+				cpuTotLbl.setBounds(900, 1050, 112, 42);
+				cpuHit.setBounds(900, 1080, 56, 16);
+				cpuStand.setBounds(900, 1080, 73, 22);
+				cpuBust.setBounds(900, 1100, 181, 25);
 
 			} catch (Exception e1) {
 				e1.printStackTrace();
@@ -231,6 +276,7 @@ public class MainGame implements ActionListener{
 					eval.updateValue(playerHand, 0);
 					removeCard();
 					updateTotLbl(0);
+					checkBust(0);
 				}else if(hitCounter==2){
 					playerCardFour = pickCard(arr);
 					playerSlot4.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+playerCardFour+".png"))));
@@ -238,6 +284,7 @@ public class MainGame implements ActionListener{
 					eval.updateValue(playerHand, 0);
 					removeCard();
 					updateTotLbl(0);
+					checkBust(0);
 				}else if(hitCounter==3){
 					playerCardFive = pickCard(arr);
 					playerSlot5.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+playerCardFive+".png"))));
@@ -245,6 +292,7 @@ public class MainGame implements ActionListener{
 					eval.updateValue(playerHand, 0);
 					removeCard();
 					updateTotLbl(0);
+					checkBust(0);
 				}else if(hitCounter==4){
 					playerCardSix = pickCard(arr);
 					playerSlot6.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+playerCardSix+".png"))));
@@ -252,6 +300,7 @@ public class MainGame implements ActionListener{
 					eval.updateValue(playerHand, 0);
 					removeCard();
 					updateTotLbl(0);
+					checkBust(0);
 				}else if(hitCounter==5){
 					playerCardSeven = pickCard(arr);
 					playerSlot7.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+playerCardSeven+".png"))));
@@ -259,46 +308,17 @@ public class MainGame implements ActionListener{
 					eval.updateValue(playerHand, 0);
 					removeCard();
 					updateTotLbl(0);
+					checkBust(0);
 				}
 			}catch (Exception e1) {
 				e1.printStackTrace();
 			}
 		}else if(e.getSource()==btnStand){
-			btnHit.setBounds(900,900,97,25);
-		    btnStand.setBounds(900,930,97,25);
-			new Thread(){
-				public void run(){
-					try{
-						for(int i=1;i<3;i++){	//fixing weird thread behavior
-							Thread.sleep(500);
-							Thread.sleep(500);
-						}
-						System.out.println("e");
-						cpuTurn.setBounds(320, 675, 181, 42);
-						Thread.sleep(3000);
-						System.out.println("f");
-						//computer move
-						cpuSlot1.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+cpuCardOne+".png"))));
-						
-					}catch(Exception e){
-						 e.printStackTrace();
-					}
-				}
-			}.start();
-		
-		    //cpuTurn.setBounds(320, 675, 181, 42);
-		    //pause
-		    //cpuTurn.setBounds(900, 960, 181, 42);
-		
-			try {
-			    Thread.sleep(1000);
-				cpuSlot1.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+cpuCardOne+".png"))));
-				Thread.sleep(1000);
-				cpuSlot1.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/blank.png"))));
-			} catch (Exception e1) {
-				
-				e1.printStackTrace();
-			}
+			//hide start game btn
+			String high = eval.getHighest()+"";
+			playerTotLbl.setText(totLbl+high);
+			playerHigh = eval.getHighest();
+			comRun();
 		}
 	}
 
@@ -321,15 +341,17 @@ public class MainGame implements ActionListener{
 	}
 
 	int cardIndex;
+	
+
 
 	private String pickCard(ArrayList<String> array){
 		cardIndex=(int)(Math.random()*arr.size());
 		return array.get(cardIndex);
 	}
-	private void removeCard(){
-		String a = arr.remove(cardIndex);
+	public void removeCard(){
+		arr.remove(cardIndex);
 	}
-	private void convertCard(String card,int turn){
+	public void convertCard(String card,int turn){
 		String cardString = card.substring(1,card.length());
 		Integer cardNumber = Integer.valueOf(cardString);
 		if(turn==0){
@@ -338,10 +360,7 @@ public class MainGame implements ActionListener{
 			cpuHand.add(cardNumber);
 		}
 	}
-
-
-
-	private void updateTotLbl(int turn){
+	public void updateTotLbl(int turn){
 		if(turn==0){
 			String total=eval.getPlayerTotal(0)+" / ";
 			for(int i=1;i<5;i++){
@@ -355,6 +374,112 @@ public class MainGame implements ActionListener{
 			}
 			playerTotLbl.setText(totLbl+total);
 		}
+		if(turn==1){
+			String total=eval.getCpuTotal(0)+" / ";
+			for(int i=1;i<5;i++){
+				if(eval.getCpuTotal(i)>0 && eval.getCpuTotal(i)<22){
+					total = total+eval.getCpuTotal(i)+" / ";
+				}
+			}
+			total = total.substring(0,total.length()-2);
+			if(eval.blackJack(1)){
+				total = "21";
+			}
+			cpuTotLbl.setText(totLbl+total);
+		}
 	}
-	
+	public void comRun(){
+		new Thread(){
+			public void run(){
+				try{
+					btnHit.setBounds(900,900,97,25);
+					btnStand.setBounds(900,930,97,25);
+					System.out.println("e");//
+					cpuTurn.setBounds(320, 675, 181, 42);
+					Thread.sleep(3000);
+					System.out.println("f");//
+					cpuSlot1.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+cpuCardOne+".png"))));
+					updateTotLbl(1);
+					cpuTotLbl.setBounds(12, 111, 112, 42);
+					Thread.sleep(2000);
+					while(eval.getCpuTotal(0)<17 && !eval.blackJack(1)){
+						cpuHit.setBounds(364, 49, 56, 16);
+						cpuHitCounter++;
+						Thread.sleep(2000);
+						cpuHit.setBounds(900, 1080, 56, 16);
+						Thread.sleep(500);
+						if(cpuHitCounter==1){
+							cpuCardThree = pickCard(arr);
+							cpuSlot3.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+cpuCardThree+".png"))));
+							convertCard(cpuCardThree,1);
+							eval.updateValue(cpuHand, 1);
+							removeCard();
+							updateTotLbl(1);
+						}else if(cpuHitCounter==2){
+							cpuCardFour = pickCard(arr);
+							cpuSlot4.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+cpuCardFour+".png"))));
+							convertCard(cpuCardFour,1);
+							eval.updateValue(cpuHand, 1);
+							removeCard();
+							updateTotLbl(1);
+						}else if(cpuHitCounter==3){
+							cpuCardFive = pickCard(arr);
+							cpuSlot5.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+cpuCardFive+".png"))));
+							convertCard(cpuCardFive,1);
+							eval.updateValue(cpuHand, 1);
+							removeCard();
+							updateTotLbl(1);
+						}else if(cpuHitCounter==4){
+							cpuCardSix = pickCard(arr);
+							cpuSlot6.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+cpuCardSix+".png"))));
+							convertCard(cpuCardSix,1);
+							eval.updateValue(cpuHand, 1);
+							removeCard();
+							updateTotLbl(1);
+						}else if(cpuHitCounter==5){
+							cpuCardSeven = pickCard(arr);
+							cpuSlot7.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+cpuCardSeven+".png"))));
+							convertCard(cpuCardSeven,1);
+							eval.updateValue(cpuHand, 1);
+							removeCard();
+							updateTotLbl(1);
+						}
+					}
+					if(eval.blackJack(1)||(eval.getCpuTotal(0)>=17&&eval.getCpuTotal(0)<=21)){
+						cpuStand.setBounds(357, 50, 73, 22);
+						Thread.sleep(1000);
+					}
+					if(eval.getCpuTotal(0)>21){
+						cpuTurn.setBounds(900, 960, 181, 42);
+						cpuBust.setBounds(318, 657, 181, 25);
+					}
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+		}.start();
+	}
+	public void checkBust(final int turn){
+		new Thread(){
+			public void run(){
+				try{
+					if(turn==0 && eval.getPlayerTotal(0)>21){
+						btnHit.setBounds(900,900,97,25);
+						btnStand.setBounds(900,930,97,25);
+						bust.setBounds(326,676,164,25);
+						btnNewGame.setBounds(1100, 340, 97, 25);
+						Thread.sleep(2000);
+						cpuSlot1.setIcon(new ImageIcon(ImageIO.read( new File("src/graphics/"+cpuCardOne+".png"))));
+						updateTotLbl(1);
+						cpuTotLbl.setBounds(12, 111, 112, 42);
+						cpuWins.setBounds(374, 708, 63, 22);
+						btnNewGame.setBounds(340, 340, 97, 25);
+					}
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+		}.start();
+	}
+
 }
